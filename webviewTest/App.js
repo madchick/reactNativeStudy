a42beaf83fb168c3d8639dcbf064aebba3c98b2a
unknown <madchick@gmail.com>
2020-06-25 01:18:03 +0900

    Add fixed webviewTest app folder

---
+++ webviewTest/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+
+export default class App extends React.Component {
+  render() {
+    return <WebView source={{ uri: 'https://www.daum.net' }} style={{ marginTop: 20 }} />;
+  }
+}
+
+
+
